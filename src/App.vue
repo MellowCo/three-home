60b7ed00f444f353d378b2e45010162d57333f72
licl <799478052@qq.com>
2022-03-02 16:15:13 +0800

    feat: 3d场景切换

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,64 @@
 
 
 <template>
-  <!-- <ThreeHome></ThreeHome> -->
+  <div class="btns">
+    <div class="btn" @click="changeMaterial('37-43')">change</div>
+    <div class="btn" @click="changeMaterial('38-42')">change2</div>
+  </div>
 
-  <div @click="changeMaterial('37-43')">change</div>
-  <!-- <Line></Line> -->
+  <div v-show="!load" id="loading">
+    <div class="progress">
+      <div class="progressbar" :style="{ transform: `scaleX(${progress})` }"></div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-// import ThreeHome from './components/ThreeHome.vue'
 import use3d from './hooks/use3d'
-const { changeMaterial } = use3d()
-
+const { progress, load, animate, changeMaterial } = use3d('37-43')
+animate()
 </script>
 
 <style>
 * {
   padding: 0;
   margin: 0;
+}
+
+#loading {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+#loading .progress {
+  margin: 1.5em;
+  border: 1px solid white;
+  width: 50vw;
+}
+#loading .progressbar {
+  margin: 2px;
+  background: white;
+  height: 1em;
+  transform-origin: top left;
+  transform: scaleX(0);
+}
+
+.btns {
+  position: fixed;
+  bottom: 5vh;
+  left: 1vw;
+  display: flex;
+}
+
+.btn {
+  padding: 10px 20px;
+  background-color: #fff;
+  margin-right: 15px;
 }
 </style>
--- src/hooks/use3d.ts
+++ src/hooks/use3d.ts
@@ -1,12 +1,10 @@
-import { MeshBasicMaterial } from 'three'
 /*
  * @Author: licl
  * @Date: 2022-03-01 15:24:51
- * @LastEditTime: 2022-03-01 16:21:50
+ * @LastEditTime: 2022-03-02 16:13:58
  * @LastEditors: licl
  * @Description:
  */
-
 import {
   Scene,
   PerspectiveCamera,
@@ -20,71 +18,137 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 import { createMaterialList } from '../utils/index'
 import config from '../config/index'
+import { Ref, ref } from 'vue'
 
-export default () => {
-  const scene = new Scene()
-  const camera = new PerspectiveCamera(
-    90,
-    window.innerWidth / window.innerHeight,
-    0.01,
-    1000
+export default (key: string) => {
+  let progress = ref<number>(0)
+  let load = ref<boolean>(false)
+
+  let scene = initScene()
+  let camera = initCamera()
+  let renderer = initRenderer()
+  let controls = initControl(camera, renderer.domElement)
+  let { loader, loadManager } = initLoader()
+  let mesh = initMesh(key, loader)
+  listenTexture(
+    loadManager,
+    scene,
+    mesh,
+    progress,
+    load,
+    render
   )
 
-  const renderer = new WebGLRenderer()
-
-  // 解决图片模糊
-  renderer.setPixelRatio(window.devicePixelRatio)
-  renderer.setSize(window.innerWidth, window.innerHeight)
-
-  camera.position.z = 0.01
-  document.body.appendChild(renderer.domElement)
-
-  //@ts-ignore
-  const controls = new OrbitControls(
-    camera,
-    renderer.domElement
-  )
-
-  const loadingManager = new LoadingManager()
-  const loader = new TextureLoader(loadingManager)
-
-  let materials: MeshBasicMaterial[]
-  let cube: Mesh
-
-  loadingManager.onLoad = () => {
-    console.log('onload')
-
-    changeMaterial('37-42')
-    cube.geometry.scale(10, 10, -10)
-    controls.update()
-    animate()
+  function render() {
+    if (load.value) {
+      renderer.render(scene, camera)
+    }
   }
 
   function animate() {
     requestAnimationFrame(animate)
     controls.update()
-    renderer.render(scene, camera)
+    render()
   }
 
   function changeMaterial(key: string) {
-    console.log(materials)
-
-    if (materials !== undefined) {
-      cube.geometry.dispose()
-      scene.remove(cube)
-    }
-
-    materials = createMaterialList(
+    load.value = false
+    progress.value = 0
+    mesh.material = createMaterialList(
       loader,
       config.home[key],
       '..'
     )
-    const geometry = new BoxGeometry(10, 10, 10)
-    cube = new Mesh(geometry, materials)
-    scene.add(cube)
   }
 
   return {
-    changeMaterial
+    changeMaterial,
+    progress,
+    load,
+    animate
   }
+}
+
+// 相机
+function initCamera() {
+  const camera = new PerspectiveCamera(
+    90,
+    window.innerWidth / window.innerHeight,
+    0.01,
+    100
+  )
+  camera.position.z = 0.01
+
+  return camera
+}
+
+// 渲染器
+function initRenderer(): WebGLRenderer {
+  const renderer = new WebGLRenderer()
+
+  // 解决图片模糊
+  renderer.setPixelRatio(window.devicePixelRatio)
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  document.body.appendChild(renderer.domElement)
+  return renderer
+}
+
+// 场景
+function initScene() {
+  const scene = new Scene()
+  return scene
+}
+
+// 控制器
+function initControl(
+  camera: PerspectiveCamera,
+  dom: HTMLCanvasElement
+) {
+  const controls = new OrbitControls(camera, dom)
+  controls.autoRotate = true
+  return controls
+}
+
+// 材质加载器
+function initLoader() {
+  const loadManager = new LoadingManager()
+  const loader = new TextureLoader(loadManager)
+  return { loader, loadManager }
+}
+
+// 监听材质加载
+function listenTexture(
+  loadManager: LoadingManager,
+  scene: Scene,
+  mesh: Mesh,
+  progress: Ref<number>,
+  load: Ref<boolean>,
+  render: Function
+) {
+  scene.add(mesh)
+
+  // 监听图片加载是否成功
+  loadManager.onLoad = () => {
+    load.value = true
+    render()
+  }
+
+  // 监听图片加载进度
+  loadManager.onProgress = (
+    urlOfLastItemLoaded,
+    itemsLoaded,
+    itemsTotal
+  ) => {
+    progress.value = itemsLoaded / itemsTotal
+  }
+}
+
+function initMesh(key: string, loader: TextureLoader) {
+  const geometry = new BoxGeometry(1, 1, 1)
+  const mesh = new Mesh(
+    geometry,
+    createMaterialList(loader, config.home[key], '..')
+  )
+  mesh.geometry.scale(1, 1, -1)
+  return mesh
 }
